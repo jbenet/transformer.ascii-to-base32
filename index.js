9484d302a32808b5aea8b1ab53b831604ed9f61a
Juan Batiz-Benet <juan@benet.ai>
2014-06-12 04:00:50 -0700

    new version

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var transformer = require('dat-transformer');
-var tAscii = transformer('ascii');
-var tBase32 = transformer('base32');
+var Conversion = require('transformer-conversion');
+var tAscii = require('transformer.ascii');
+var tBase32 = require('transformer.base32');
 // require any other modules you may need here.
 var base32 = require('base32');
 
-module.exports = transformer.Conversion(tAscii, tBase32, convert);
+module.exports = Conversion(tAscii, tBase32, convert);
 
 function convert(ascii) {
   return base32.encode(ascii);
